feb9446543a2380b5ca0a2db6daaee1562ae4cbd
Carlo Nazareno <csnazareno94@gmail.com>
2020-02-05 21:25:08 -0800

    working city weather and weather cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,47 @@
+let date = moment().format('YYYY-MM-DD')
 
-document.addEventListener('keydown', event => {
+document.getElementById('search-btn').addEventListener('click', event => {
   event.preventDefault()
-  if(event.keycode === '13') {
-    let city = document.getElementById('city-search').value
-    fetch(`http://api.weatherapi.com/v1/current.json?key=1494b6e914a84eb4aeb215103200502&q=${city}`)
-      .then(r => r.json())
-      .then(weather => {
-        console.log(weather)
-      })
-      .catch(e => console.error(e))
-  }
-})
-  
+  console.log('ping')
 
+  fetch(`http://api.weatherapi.com/v1/forecast.json?key=1494b6e914a84eb4aeb215103200502&q=${document.getElementById('search-bar').value}&days=5`)
+  .then(r => r.json())
+  .then(weather => {
+    // City Weather Details
+    document.getElementById('current-city-weather').innerHTML = ' '
+    let cityWeather = document.createElement('div')
+    cityWeather.className = ''
+    cityWeather.id = ''
+    cityWeather.innerHTML = `
+    <h1 class="font-weight-bold">${weather.location.name} - ${weather.location.localtime}</h1>
+    <p>Temparature: ${weather.current.temp_f}°</p>
+    <p>Humidity: ${weather.current.humidity}</p>
+    <p>Wind Speed: ${weather.current.wind_mph}</p>
+    <p>UV Index: ${weather.current.uv}</p>
+    `
+    document.getElementById('current-city-weather').append(cityWeather)
+  // Weather Cards
+    document.getElementById('weather-cards').innerHTML = ' '
+    for (let i = 0; i < weather.forecast.forecastday.length; i++) {
+      let weatherCard = document.createElement('div')
+      weatherCard.className = 'weather-cards col-lg-2 col-md-2 col-sm-6'
+      weatherCard.id = 'weather-cards-rendered'
+      weatherCard.innerHTML = `
+      <div class="card">
+      <div class="card-header">
+        ${weather.forecast.forecastday[i].date}
+      </div>
+      <div class="card-body">
+        <p>${weather.forecast.forecastday[i].day.condition.text}</p>
+        <p><img src="https:${weather.forecast.forecastday[i].day.condition.icon}" height:"64" width:"64"></p>
+        <p>Temp: ${weather.forecast.forecastday[i].day.maxtemp_f}°</p>
+        <p>Humidity: ${weather.forecast.forecastday[i].day.avghumidity}</p>
+      </div>
+    </div>
+      `
+      document.getElementById('weather-cards').append(weatherCard)
+    }
+  })
+  .catch(e => console.error(e))
+})
+ 
